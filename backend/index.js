8b9db4328f1b053189237abe0f35f0e7489aff8d
siddhantchouhan1 <82711981+siddhantchouhan1@users.noreply.github.com>
2026-06-27 21:06:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -163,7 +163,7 @@ main()
 async function main() {
   await mongoose.connect(uri);
 };
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
 });
 
